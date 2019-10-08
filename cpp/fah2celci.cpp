0ac573a2dbb8e0315dfda5a96e090393a9b5c666
Warlock001 <alfinwilliam@gmail.com>
2019-10-08 19:56:32 +0530

    error check

--- cpp/fah2celci.cpp
+++ cpp/fah2celci.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main(){
     float v,a;
-    cout<<"Fahrenheit to Celcius , 1\nClecius to Fahrenheit , 2";
+    cout<<"For Fahrenheit to Celcius : 1\nFor Clecius to Fahrenheit : 2\n";
     cin>>a;
-    cout<<"Enter value";
-    cin>>v;
     if(a!=1 && a!=2){
-        cout<<"Invalid option";
+        cout<<"Invalid option\n";
         exit(0);
     }
+    cout<<"Enter value\n";
+    cin>>v;
     cout<<( a==1? ((v - 32.0) * 5.0 / 9.0) : ((v * 9.0) / 5.0 + 32))<<endl;
     }
