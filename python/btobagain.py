c357828996b54ffd71935a70212913c91753cf7c
alfinwilliam <alfinwilliam@gmail.com>
2020-10-29 23:42:20 +0530

    restart study

---
+++ python/btobagain.py
@@ -0,0 +1,20 @@
+print("Hello World")
+# single line comment
+"""
+this is a 
+multiline comment
+"""
+print(5 > 2)
+print("alfin" + "william")
+print("alfin" * 2)
+
+x = 5
+y = "John"
+
+print(x)
+print(y)
+
+x = 5
+x = 'sally'
+# now datatype of x is str
+
