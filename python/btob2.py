bd8fcfedaff5e37bdf36ef3656c389a654877b10
alfinwilliam <alfinwilliam@gmail.com>
2020-10-31 01:31:08 +0530

    moving on to operators

--- python/btob2.py
+++ python/btob2.py
@@ -27,3 +27,48 @@ print(x)
 
 x = b + b  #string concat
 print(x)
+
+
+
+# using format function we can add int values to string.
+
+i = "alfin is a good {}"
+b= "boy"
+print(i.format(b))
+
+m = "sharlet"
+s = "shanu"
+so = "alfin"
+
+endstat = "Wishal consists of {} , {} , and {}"
+print(endstat.format(m,s,so))
+
+a = 1
+b = 2
+c = 3
+order = "after {1} comes {2} then {0}"
+print(order.format(c,a,b))
+
+
+
+#use escape character in front of illegal characters in a string
+
+print("this is \" illegal to do inside a \" string")
+
+#string count
+
+stre = " rrrrrrrrrrr"
+
+print(stre.count('r'))
+
+print(stre.islower())
+
+#boolean.. use to evaluate an expression
+
+# Almost any value is evaluated to True if it has some sort of content.
+
+def myfunction():
+    return True
+
+if myfunction():
+    print("YES")
