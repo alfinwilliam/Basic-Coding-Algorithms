ecc436ea366275657ac4e0daea00cec0896f5c71
alfinwilliam <alfinwilliam@gmail.com>
2020-11-01 02:48:03 +0530

    third file

---
+++ python/btob3.py
@@ -0,0 +1,32 @@
+def myfn():
+    return True
+
+if myfn():
+    print("YES")
+else:
+    print("No")
+
+
+# python isinstance.. to check whether same datatype
+
+x = 200
+print(isinstance(x, int))
+
+#python Operators
+
+# 1. Arithmetic Operators
+
+#arithmetic operators are used with numeric values to perform common mathematical operations
+
+print(2+3) #addition
+print(3-2) #subtraction
+print(2*3) #multiplication
+print(2/3) #division
+print(3%2) #modulus
+print(3**2) #exponentiation
+print(3 // 2) #floor division
+
+# 2. Assignment Operators
+
+#assignment operators are used to assign values to variables.
+
