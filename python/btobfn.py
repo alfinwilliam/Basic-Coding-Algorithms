758064fec470d725e597f525070592a257b8ddcb
alfinwilliam <alfinwilliam@gmail.com>
2020-11-04 23:48:46 +0530

    fn commit

---
+++ python/btobfn.py
@@ -0,0 +1,9 @@
+# fn is a block of code that contains arguments and return values. They are useful for code modularity and reusability
+# also good for error finding.
+# you can pass data as parameters to a function. It can also return data as a result.
+
+def sum(a,b):
+    s = a + b
+    return s
+
+print(sum(7,2))
