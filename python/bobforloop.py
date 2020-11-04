e7a4fa2c816f4c9182c60e171042c447f90ea21f
alfinwilliam <alfinwilliam@gmail.com>
2020-11-04 22:38:56 +0530

    next day of interview

---
+++ python/bobforloop.py
@@ -0,0 +1,23 @@
+fruits = ["apple","banana","cherry"]
+for x in fruits:
+    if x == "banana":
+        continue
+    print(x)
+
+x = "banana"
+for a in x:
+    print(a)
+
+a = [1,2,3]
+
+for x in a:
+    print(x)
+    if x == 2:
+        break
+
+# range function in python
+
+#to loop through a set of code for a specific number of time , we use range function.
+
+# range() function returns a sequence of numbers , starting from 0 to till the specified number.
+
--- python/btobwhile.py
+++ python/btobwhile.py
@@ -1,6 +1,26 @@
+#two primitive loop commands.. while and for
+
 a = 1 
 sum = 0
 while(a<10):
     sum = sum + a
     a = a + 1
 print(sum)
+
+#using break statement
+
+a = 1
+while(a<6):
+    print(a)
+    if(a == 3):
+        break
+    a+=1
+
+#using continue statement
+
+a = 1
+while(a<6):
+    a+=1
+    if(a == 3):
+        continue
+    print(a)
